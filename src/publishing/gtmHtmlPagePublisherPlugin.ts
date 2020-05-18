fba582b60ebf3f2a48cfcc897802614cf0537179
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-05-17 17:04:54 -0700

    Improvements around logging, site and integrations settings.

--- src/publishing/gtmHtmlPagePublisherPlugin.ts
+++ src/publishing/gtmHtmlPagePublisherPlugin.ts
@@ -1,19 +1,23 @@
 import { HtmlPagePublisherPlugin } from "@paperbits/common/publishing";
 import { ISiteService } from "@paperbits/common/sites";
+import { GoogleTagManagerSettings } from "./gtmSettings";
 
 
 export class GoogleTagManagerHtmlPagePublisherPlugin implements HtmlPagePublisherPlugin {
     constructor(private readonly siteService: ISiteService) { }
 
     public async apply(document: Document): Promise<void> {
-        const settings = await this.siteService.getSiteSettings();
+        const gtmSettings = await this.siteService.getIntegrationSettings<GoogleTagManagerSettings>("googleTagManager");
 
-        if (!settings?.integration?.googleTagManager) {
+        if (!gtmSettings) {
             return;
         }
 
-        const gtmSettings = settings.integration.googleTagManager;
         const containerId = gtmSettings.webContainerId || gtmSettings.containerId;
+
+        if (!containerId) {
+            return;
+        }
 
         const headScriptElement = document.createElement("script");
 
@@ -22,12 +26,12 @@ export class GoogleTagManagerHtmlPagePublisherPlugin implements HtmlPagePublishe
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${settings.integration.googleTagManager.containerId}');`;
+        })(window,document,'script','dataLayer','${containerId}');`;
         document.head.insertAdjacentElement("afterbegin", headScriptElement);
 
         const bodyScriptElement = document.createElement("noscript");
         bodyScriptElement.innerHTML = `
-        <iframe src="https://www.googletagmanager.com/ns.html?id=${settings.integration.googleTagManager.containerId}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+        <iframe src="https://www.googletagmanager.com/ns.html?id=${containerId}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
         document.body.insertAdjacentElement("afterbegin", bodyScriptElement);
     }
 }
